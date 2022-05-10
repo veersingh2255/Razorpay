e65fe66a67d7a2ef800309b8980f5f271362911f
veersingh2255 <veersingh08830@gmail.com>
2022-05-10 23:27:51 +0530

    changes

--- Coffee/settings.py
+++ Coffee/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6c425zu&*5%(i$mzxx74mb-io@%1ufgc1rke(gfx@ib$gbuugn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["razorpaycoffee.herokuapp.com","localhost"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'static'
